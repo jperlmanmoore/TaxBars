1ffa662041d53ca4039c1f485ca7344f807b3ea1
JacobBraxley <jacobbraxley@gmail.com>
2019-04-11 19:10:56 -0400

    reorg, using right query

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,11 +5,6 @@
 
 //Jacob
 
-//Jennifer
-
-//Hilary
-
-
 // BEA 
 // User Guide: https://apps.bea.gov/api/_pdf/bea_web_service_api_user_guide.pdf
 // Query example: https://apps.bea.gov/api/data/?&UserID=41900668-00C1-4805-8EA7-643EE7CB9BE9&method=GETDATASETLIST&
@@ -19,14 +14,23 @@
 // The above call requires a Year, a frequency (Annual, Monthly, Quartly), the table name, the data set, the method, and our apikey.
 // response.BEAAPI.Results.Data
 // TODO - Determine how to see what is a subcategory of what.
-$.ajax({
-    url: `https://apps.bea.gov/api/data/?&UserID=41900668-00C1-4805-8EA7-643EE7CB9BE9&method=GETparameterLIST&datasetname=Regional`,
-    method: "GET"
-}).then(function (response) {
-    console.log(response.BEAAPI.Results.Data);
-});
+function logBEA() {
+    $.ajax({
+        url: `https://apps.bea.gov/api/data/?&UserID=41900668-00C1-4805-8EA7-643EE7CB9BE9&method=getdata&frequency=a&year=2017&tablename=t31600&datasetname=nipa`,
+        method: "GET"
+    }).then(function (response) {
+        debugger;
+        console.log(response.BEAAPI.Results.Data);
+    });
+}
 
 $(document).ready(function() {
     $('.collapsible').collapsible();
     $('.datepicker').datepicker();
 })
+
+//Jennifer
+
+//Hilary
+
+
